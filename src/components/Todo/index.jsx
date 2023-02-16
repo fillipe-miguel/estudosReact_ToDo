04eace681eb4917733dca97694d97bab706f939b
Fillipe <fillipeolipe@gmail.com>
2023-02-16 14:09:00 -0300

    Feito lista separadas de a fazer e feitas

--- src/components/Todo/index.jsx
+++ src/components/Todo/index.jsx
@@ -12,11 +12,19 @@ import './style.css';
 const Todo = () => {
   const items = useSelector((state) => state);
 
+  const itemsDone = items.filter((item) => item.done);
+  const itemsNotDone = items.filter((item) => !item.done);
+  console.log('🚀 ~ file: index.jsx:17 ~ Todo ~ itemsNotDone', itemsNotDone);
+  console.log('🚀 ~ file: index.jsx:16 ~ Todo ~ itemsDoned', itemsDone);
+
   return (
     <Container>
       <h1 className="todo-title">Todo List NOVO</h1>
       <FormAddTodo></FormAddTodo>
-      <TodoList title="TODOS">{items}</TodoList>
+      {itemsNotDone.length > 0 && (
+        <TodoList title="A fazer">{itemsNotDone}</TodoList>
+      )}
+      {itemsDone.length > 0 && <TodoList title="Feitos">{itemsDone}</TodoList>}
     </Container>
   );
 };
